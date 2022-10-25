fa953edc97f94806f0aa924f635efecec78519cb
iara gerardi <iaragerardi0207@gmail.com>
2022-10-25 20:54:26 -0300

    cambios

---
+++ Node/FCC/httpResponses.js
@@ -0,0 +1,8 @@
+// Las respuestas http incluyen: un codigo y texto de estado, la version de HTTP, headers y body de la peticion
+
+/*Codigos de estado HTTP:
+-Informativas (100-199)
+-Satifactoria (200 -299)
+-Redirecciones (300-399)
+-Errores de los clientes (400-499)
+-Errores de los servidores (500-599)*/
